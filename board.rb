cf825fd06b4128bba944d520ae6315a8203908b2
pandenok <dimitri.shatunin@gmail.com>
2020-07-26 13:13:40 +0200

    Add visualization of the route incl starting point

--- board.rb
+++ board.rb
@@ -9,7 +9,6 @@ class Board
   end
   
   def build_board
-      # @coordinates = board.first.each_index.to_a.product(board.each_index.to_a)
     Array.new(SIZE) {Array.new(SIZE, " ")}
   end
 
@@ -18,34 +17,25 @@ class Board
     until queue.empty?
       current = queue.shift
       return current if current.x == trg.x && current.y == trg.y
-      current.where_can_jump_from.each { |child| queue << child }
+      current.where_can_jump.each { |child| queue << child }
     end
   end
   
-  def knights_moves(from, to)
+  def knight_moves(from, to)
     src = Knight.new(*from)
     trg = Knight.new(*to)
-    path = bfs_traversal(src, trg)
-    p path
-    p route(path)
-    # route = []
-    # route.unshift([trg.x, trg.y])
-    # current = path.parent
-    # until current.nil?
-    #   route.unshift [current.x, current.y]
-    #   current = current.parent
-    # end
-    # puts "You made it in #{route.length - 1} moves! Here's your path:"
-    # # puts "#{route} : "
-    # route.each { |move| puts "=> #{move}" }
-    
-    # p route
-    # # puts route.each { |move| move }
-    # return nil
+    traversal = bfs_traversal(src, trg)
+    route = route(traversal)
+    route.each { |move| place_knight(*move) }
+    puts "\nYou made it in #{route.length - 1} moves! Here's your path:"
+    route.each { |move| puts " => #{move}" }
   end
 
   def route(node, route = [])
-    return route if node.parent.nil?
+    if node.parent.nil?
+      route << [node.x, node.y]
+      return route 
+    end
     route(node.parent, route)
     route << [node.x, node.y]
   end
@@ -55,7 +45,7 @@ class Board
   end
 
   def show
-    puts "    0   1   2   3   4   5   6   7  "
+    puts "\n    0   1   2   3   4   5   6   7  "
     puts "  +---+---+---+---+---+---+---+---+"
     board.each_with_index do |inner, idx| 
       puts "#{idx} | " + inner.each { |cell| cell}.join(' | ') + " |"
